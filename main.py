274f906373be7e7bf98534a3b41c5dd7a95040b7
Thomas George <private@thomas-george.com>
2023-04-08 11:41:10 +0200

    ajout des murs horizontaux

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ while running:
     screen.blit(game.player.image, game.player.rect)
 
     # Check action du joueur
-    if game.pressed.get(pygame.K_RIGHT):
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < screen.get_width():
         game.player.move_right()
-    elif game.pressed.get(pygame.K_LEFT):
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
         game.player.move_left()
 
     # Refresh écran
